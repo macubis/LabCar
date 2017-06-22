abefde1c930a3d272e4c6bc158d3ca016370ba77
macubis <macubis@gmail.com>
2017-06-22 10:49:36 -0400

    Subiendo ejercicio final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-main.js
+
+    var inputPartida =(document.getElementById(“punto-partida”));    
+    var autocompleteOrigen = new google.maps.places.Autocomplete(inputPartida);
+    autocompleteOrigen.bindTo(‘bounds’, map);
+    
+    var inputLlegada = document.getElementById(“punto-llegada”);
+    var autocompleteDestino = new google.maps.places.Autocomplete(inputLlegada);
+    autocompleteDestino.bindTo(‘bounds’, map);
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true}); 
+    directionsDisplay.setMap(map);
